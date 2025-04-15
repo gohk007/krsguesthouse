a34392813c26020cf2c342b6ec5e504e6a3c0895
Koushik KS <44638962+kskoushik26@users.noreply.github.com>
2025-04-16 00:37:26 +0530

    added customer review section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,11 +6,7 @@ const Contact = () => (
     <h2>How to Book Rooms</h2>
     <ol>
       <li>Call us at <a href="tel:+919448734152"><strong>+91-9448734152</strong></a> to check room availability.</li>
-      <li>If rooms are available, proceed with the payment via 
-        <a href="https://payments.google.com" target="_blank" rel="noopener noreferrer"><strong>GPay</strong></a> 
-        or 
-        <a  href="https://www.phonepe.com/" target="_blank" rel="noopener noreferrer"><strong>PhonePe</strong></a>.
-      </li>
+      <li>If rooms are available, proceed with the payment via Google pay or phonepe</li>
       <li>After payment, send a screenshot of the transaction, along with your Aadhar card, member details, and the date of stay through WhatsApp.</li>
       <li>Confirm the booking on the call with us.</li>
       <li>On the day of check-in, give us a call, and you will be provided with your room number.</li>
